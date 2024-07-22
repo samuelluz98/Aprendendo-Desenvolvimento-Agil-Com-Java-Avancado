b4c0c7935d5a0b4ff26dfcb495965c7ba3b91e85
José Samuel Assunção luz <43012827+samuelluz98@users.noreply.github.com>
2024-07-22 19:17:04 -0300

    Criando o primeiro Servlet

---
+++ OlaWeb/src/java/OlaServlet.java
@@ -0,0 +1,25 @@
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/Ola")
+public class OlaServlet extends HttpServlet {
+
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String nome = request.getParameter("nome"); 
+        if(nome == null){
+            nome = "camarada";
+        }
+        response.getWriter().print("<html><h1>Oi " + nome + " seu programadorzão da porra!</h1><html>");
+    }
+
+
+}
